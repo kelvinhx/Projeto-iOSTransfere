39bebc9552a923217305a9ff8512bc65447f5a0a
kelvinhx <rebeijar@gmail.com>
2026-06-04 15:32:18 -0300

    Criar AppConfig.kt

---
+++ app/src/main/java/com/projeto/mobilecloud/AppConfig.kt
@@ -0,0 +1,9 @@
+package com.projeto.mobilecloud
+
+object AppConfig {
+    const val SERVER_PORT = 8080
+    const val APP_NAME = "Nexus Explorer Pro"
+    const val ROOT_PATH = "/storage/emulated/0"
+    val THEME_ACCENT = "#007AFF" // Azul iOS
+    val THEME_BG = "#08080A"
+}
