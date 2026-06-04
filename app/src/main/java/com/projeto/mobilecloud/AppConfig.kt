62cbe4080e4896ce9487f308fb516edd146e080b
kelvinhx <rebeijar@gmail.com>
2026-06-04 20:40:13 -0300

    Atualizar o AppConfig.kt

--- app/src/main/java/com/projeto/mobilecloud/AppConfig.kt
+++ app/src/main/java/com/projeto/mobilecloud/AppConfig.kt
@@ -2,8 +2,7 @@ package com.projeto.mobilecloud
 
 object AppConfig {
     const val SERVER_PORT = 8080
-    const val APP_NAME = "Nexus Explorer Pro"
     const val ROOT_PATH = "/storage/emulated/0"
-    val THEME_ACCENT = "#007AFF" // Azul iOS
-    val THEME_BG = "#08080A"
+    const val THEME_ACCENT = "#007AFF" 
+    const val THEME_BG = "#08080A"
 }
