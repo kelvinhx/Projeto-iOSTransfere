e8649b481e1928c6047ae680998224ade6c8660b
kelvinhx <rebeijar@gmail.com>
2026-06-04 20:42:32 -0300

    Atualizar o Logger.kt

--- app/src/main/java/com/projeto/mobilecloud/Logger.kt
+++ app/src/main/java/com/projeto/mobilecloud/Logger.kt
@@ -4,14 +4,10 @@ import java.util.*
 
 object Logger {
     private val logs = LinkedList<String>()
-
     fun log(message: String) {
         val time = java.text.SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
-        val entry = "[$time] $message"
-        logs.addFirst(entry)
-        if (logs.size > 100) logs.removeLast() // Mantém apenas os últimos 100
-        println(entry)
+        logs.addFirst("[$time] $message")
+        if (logs.size > 50) logs.removeLast()
     }
-
     fun getLogs(): String = logs.joinToString("\n")
 }
