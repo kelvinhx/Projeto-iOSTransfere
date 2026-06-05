3b940ba5bbfcb113f658beb21db56d0cd634710f
kelvinhx <rebeijar@gmail.com>
2026-06-04 22:57:45 -0300

    Atualizar o AppConfig.kt

--- app/src/main/java/com/projeto/mobilecloud/AppConfig.kt
+++ app/src/main/java/com/projeto/mobilecloud/AppConfig.kt
@@ -3,9 +3,13 @@ package com.projeto.mobilecloud
 object AppConfig {
     const val SERVER_PORT = 8080
     const val ROOT_PATH = "/storage/emulated/0"
-    const val THEME_ACCENT = "#007AFF"
     
-    // INFRAESTRUTURA
-    const val BUFFER_SIZE = 65536 // 64KB para estabilidade em arquivos grandes
-    const val SESSION_PIN = "1234" // PIN simples para autorizar o iPhone
+    // Cores Premium
+    const val COLOR_BG = "#050505"
+    const val COLOR_CARD = "#121214"
+    const val COLOR_ACCENT = "#0A84FF" // Blue iOS
+    const val COLOR_SUCCESS = "#30D158" // Green iOS
+    const val COLOR_DANGER = "#FF453A" // Red iOS
+    
+    const val BUFFER_SIZE = 65536
 }
