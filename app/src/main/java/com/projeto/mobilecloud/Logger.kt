3e43749e5c70200b05e9623a078f8180ab37b616
kelvinhx <rebeijar@gmail.com>
2026-06-04 22:37:19 -0300

    Atualizar o Logger.kt

--- app/src/main/java/com/projeto/mobilecloud/Logger.kt
+++ app/src/main/java/com/projeto/mobilecloud/Logger.kt
@@ -1,13 +1,25 @@
 package com.projeto.mobilecloud
 
+import android.app.ActivityManager
+import android.content.Context
 import java.util.*
 
 object Logger {
     private val logs = LinkedList<String>()
+
     fun log(message: String) {
         val time = java.text.SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
         logs.addFirst("[$time] $message")
-        if (logs.size > 50) logs.removeLast()
+        if (logs.size > 100) logs.removeLast()
     }
+
+    fun getSystemStatus(context: Context): String {
+        val actManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val memInfo = ActivityManager.MemoryInfo()
+        actManager.getMemory(memInfo)
+        val ramLivre = memInfo.availMem / 1024 / 1024
+        return "RAM Livre: ${ramLivre}MB | Low RAM: ${memInfo.lowMemory}"
+    }
+
     fun getLogs(): String = logs.joinToString("\n")
 }
