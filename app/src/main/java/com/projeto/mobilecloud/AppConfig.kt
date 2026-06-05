b357abb29c02393d05b73b9d2ff692d8a0b136f5
kelvinhx <rebeijar@gmail.com>
2026-06-04 21:59:33 -0300

    Atualizar o AppConfig.kt

--- app/src/main/java/com/projeto/mobilecloud/AppConfig.kt
+++ app/src/main/java/com/projeto/mobilecloud/AppConfig.kt
@@ -3,6 +3,5 @@ package com.projeto.mobilecloud
 object AppConfig {
     const val SERVER_PORT = 8080
     const val ROOT_PATH = "/storage/emulated/0"
-    const val THEME_ACCENT = "#007AFF" 
-    const val THEME_BG = "#08080A"
+    const val THEME_ACCENT = "#007AFF"
 }
