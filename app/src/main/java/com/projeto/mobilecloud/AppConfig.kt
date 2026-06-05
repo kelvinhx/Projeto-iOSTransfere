64f8b25173046550d893c7320fef739d81155d5d
kelvinhx <rebeijar@gmail.com>
2026-06-04 22:36:31 -0300

    Atualizar o AppConfig.kt

--- app/src/main/java/com/projeto/mobilecloud/AppConfig.kt
+++ app/src/main/java/com/projeto/mobilecloud/AppConfig.kt
@@ -4,4 +4,8 @@ object AppConfig {
     const val SERVER_PORT = 8080
     const val ROOT_PATH = "/storage/emulated/0"
     const val THEME_ACCENT = "#007AFF"
+    
+    // INFRAESTRUTURA
+    const val BUFFER_SIZE = 65536 // 64KB para estabilidade em arquivos grandes
+    const val SESSION_PIN = "1234" // PIN simples para autorizar o iPhone
 }
